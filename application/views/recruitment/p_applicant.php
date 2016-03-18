506e13338b7a330fe19e88d101cd6aeefcc7f70a
wiliamdecosta <wiliamdecosta@gmail.com>
2016-03-19 00:05:47 +0700

    ''

--- application/views/recruitment/p_applicant.php
+++ application/views/recruitment/p_applicant.php
@@ -328,6 +328,9 @@
 
                     form.data('styled', true);
                 },
+                afterShowForm: function(form) {
+                    form.closest('.ui-jqdialog').center();
+                },
                 onClick: function (e) {
                     //alert(1);
                 },
@@ -347,6 +350,8 @@
                     var form = $(e[0]);
                     form.closest('.ui-jqdialog').find('.ui-jqdialog-title').wrap('<div class="widget-header" />')
                     style_search_form(form);
+                    
+                    form.closest('.ui-jqdialog').center();
                 },
                 afterRedraw: function () {
                     style_search_filters($(this));
