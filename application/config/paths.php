ceca60ecbdf777584bd159bdaf6a5a158ab681fc
wiliamdecosta <wiliamdecosta@gmail.com>
2016-03-22 22:41:57 +0700

    ''

--- application/config/paths.php
+++ application/config/paths.php
@@ -17,6 +17,7 @@ $config['BS_PATH'] = define('BS_PATH',base_url().'application/third_party/assets
 $config['BS_CSS_PATH'] = define('BS_CSS_PATH',base_url().'application/third_party/assets/css/');
 $config['BS_JS_PATH'] = define('BS_JS_PATH',base_url().'application/third_party/assets/js/');
 
+$config['UPLOAD_PATH'] = define('UPLOAD_PATH',base_url().'application/third_party/applicant_docs/');
 
 /* End of file autoload.php */
 /* Location: ./application/config/paths.php */
