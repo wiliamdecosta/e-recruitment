9ba3d5de4dc876e92403262dcee950872b6f5c25
wiliamdecosta <wiliamdecosta@gmail.com>
2016-04-01 11:38:36 +0700

    ''

--- application/config/routes.php
+++ application/config/routes.php
@@ -37,8 +37,7 @@
 | in the URL cannot be matched to a valid route.
 |
 */
-
-$route['default_controller'] = "base/index";
+$route['default_controller'] = "admin/index";
 $route['404_override'] = '';
 
 
